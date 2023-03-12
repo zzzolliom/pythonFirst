0442f25686ef842e0683f5089fb41ced0aa73c15
zzzolliom <zubolga419@gmail.com>
2023-03-12 23:01:28 +0300

    12.03 Домашка №2 задание1

---
+++ 0.10Lesson.py
@@ -0,0 +1,6 @@
+def append_default(element,lst=[]):
+    lst.append(element)
+    return  lst
+
+print(append_default('1'))
+print(append_default(12))
---
+++ 12.03_Home_task2a.py
@@ -0,0 +1,17 @@
+name_months = {1: 'jan', 2: 'feb', 3: 'march', 4: 'april', 5: 'may', 6: 'jun', 7: 'jul', 8: 'aug', 9: 'sep', 10: 'okt', 11: 'nov', 12: 'dec'}
+year=int(input("Введите год для проверки:"))
+n=int(input("Введите номер месяца"))
+if(year%4==0 and year%100!= 0 or year%400==0):
+    quantity_days_in_month = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31,6: 30, 7: 31, 8: 31, 9: 30, 10: 31,11: 30, 12: 31}
+    print(name_months[n], quantity_days_in_month[n])
+
+else:
+    quantity_days_in_month = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31,6: 30, 7: 31, 8: 31, 9: 30, 10: 31,11: 30, 12: 31}
+    print(name_months[n], quantity_days_in_month[n],'days')
+
+
+
+#1. Напишите программу, считывающую одно число N - номер месяца, и выводящую, сколько в  этом месяце дней и его название. (используйте словари)
+
+
+
