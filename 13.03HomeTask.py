1744b1c163203852092d69a4eb62a687a52c51c1
zzzolliom <zubolga419@gmail.com>
2023-03-14 19:45:58 +0300

    12.03 Домашка №2 задание2 исправила название переменной

---
+++ 13.03HomeTask.py
@@ -0,0 +1,17 @@
+"""
+размер вклада=n
+срок вклада=m
+процент =10%
+"""
+
+"""def big_money(n,m,lst[])
+    n=n*0.01+n
+    return 1 if m==0 else big_money(n,m-1)
+    lst.append(n)
+    return lst
+print(big_money())"""
+
+
+def mu (x):
+    return x*2
+print(list(map(mu,[1,2,3,4])))
---
+++ 14.03.py
@@ -0,0 +1,8 @@
+def is_pos_neg(x):
+    if x<0:
+         print("Positive")
+    elif x>0:
+        print("Negotive")
+    else:
+        print("0")
+print(is_pos_neg(2))
