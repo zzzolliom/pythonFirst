a6413eaf07f7d7c8a1c691caa4099ca6415c74dc
zzzolliom <zubolga419@gmail.com>
2023-03-17 00:10:23 +0300

    ДЗ А с 4 урока

---
+++ HT_3/HomeTask_4_A.py
@@ -0,0 +1,8 @@
+first_digit=int(input())
+second_digit=int(input())
+first_str=str(input())
+
+def calculate_output (x,y,z):
+    print(x, y,z)
+
+calculate_output(first_digit,first_str,second_digit)
