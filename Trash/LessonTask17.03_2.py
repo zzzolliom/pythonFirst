20267255a5c0eb62cfd42540d43aecfb6561e6dd
zzzolliom <zubolga419@gmail.com>
2023-03-17 21:31:30 +0300

    исправление кода

--- Trash/LessonTask17.03_2.py
+++ Trash/LessonTask17.03_2.py
@@ -1,9 +1,6 @@
 import random
 
-throw= (random.randint(1,6))
-throw2= (random.randint(1,6))
-print(throw, throw2)
-answer= input("Еще раз?")
+answer="Yes"
 while answer=="Yes":
     throw = (random.randint(1, 6))
     throw2 = (random.randint(1, 6))
