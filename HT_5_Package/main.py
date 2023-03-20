27efe24014be356387a0e939c2393f17d3444dab
zzzolliom <zubolga419@gmail.com>
2023-03-20 21:54:14 +0300

    тесты

--- HT_5_Package/main.py
+++ HT_5_Package/main.py
@@ -7,3 +7,6 @@ banknotes1 = input("Введите купюры внесенные в кассу
 banknotes1 = banknotes1.split()
 banknotes1 = [float(i) for i in banknotes1]
 print(cash_register(banknotes1, price1))
+
+
+#{5000, 1000, 500, 100, 50, 10, 5, 2, 1, 0.50, 0.10, 0.05, 0.01}
--- HT_5_Package/test_cash_registr.py
+++ HT_5_Package/test_cash_registr.py
@@ -1,2 +1,33 @@
 import pytest
-from utils import cash_register
+from HT_5_Package.utils import cash_register
+
+
+def test_cash_register_prise_one_bancknote():
+    a = [50]
+    b = 1.74
+    result = cash_register(a, b)
+    assert result == [10, 10, 10, 10, 5, 2, 1, 0.1, 0.1, 0.05, 0.01]
+
+def test_cash_register_all_bancknotes():
+    a = [5000, 1000, 500, 100, 50, 10, 5, 2, 1, 0.50, 0.10, 0.05, 0.01]
+    b = 6668.66
+    result = cash_register(a, b)
+    assert result == []
+
+def test_cash_register_without_dot():
+    a = [5000, 5000, 5000, 5000, 5000]
+    b = 23500
+    result = cash_register(a, b)
+    assert result == [1000, 500]
+
+def test_cash_register_prise_0():
+    a = [10, 10, 10, 10, 0.5]
+    b = 0
+    with pytest.raises(ValueError):
+        cash_register(a, b)
+
+def test_cash_register_not_enough():
+    a = [10, 10, 10, 10, 0.5]
+    b = 1000
+    with pytest.raises(ValueError):
+        cash_register(a, b)
