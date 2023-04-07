27a57a54d4a2dd999e6f6f3d920a726353d458cf
zzzolliom <zubolga419@gmail.com>
2023-04-07 09:55:06 +0300

    калькулятор

--- HT_3/HomeTask_4_A.py
+++ HT_3/HomeTask_4_A.py
@@ -1,8 +1,72 @@
-first_digit=int(input())
-second_digit=int(input())
-first_str=str(input())
+def get_input_data():
+    first_number = input('число')
+    sign = input('знак')
+    second_number = input('число')
+    try:
+        first_number = int(first_number)
+        second_number = int(second_number)
+    except:
+        print("тут просто калькулятор, надо было вводить число")
+        get_input_data()
+    try:
+        if sign == "=":
+            sign = input('Введи другой знак, тут калькулятор')
+        elif sign == '+':
+            result = first_number + second_number
+        elif sign == '-':
+            result = first_number - second_number
+        elif sign == '*':
+            result = first_number * second_number
+        elif sign == '/':
+            if second_number == 0:
+                result = "На 0 делить нелзя, давай заново"
+                get_input_data()
+            else:
+                result = first_number / second_number
+        print(result)
+        calculator(result)
+    except:
+        print("Это калькулятор, надо было вводить знак")
+        get_input_data()
 
-def calculate_output (x,y,z):
-    print(x, y,z)
 
-calculate_output(first_digit,first_str,second_digit)
+
+
+
+def calculator(result):
+    while True:
+        sign = input("Введите знак")
+        if sign !="+" and sign !="+" and sign !="+" and sign !="+":
+            print("Это калькулятор, тыт производятся элементарные вычисления с числами, соберись")
+        else:
+            first_number = result
+        # print(f"first_number на 34 строке = {first_number}")
+            second_number = input("Число")
+            try:
+                second_number = int(second_number)
+            except:
+                print("Это калькулятор, тыт производятся элементарные вычисления с числами, соберись")
+            try:
+                if sign == '+':
+                  result = first_number + second_number
+                  print(result)
+                elif sign == '-':
+                    result = first_number - second_number
+                    print(result)
+                elif sign == '*':
+                    result = first_number * second_number
+                    print(result)
+                elif sign == '/':
+                    if second_number == 0:
+                        print("На 0 делить нелзя")
+                    else:
+                        result = first_number / second_number
+                    print(result)
+                elif sign=="=":
+                    print(result)
+            except:
+                print("Это калькулятор, тыт производятся элементарные вычисления с числами, соберись")
+
+
+
+get_input_data()
